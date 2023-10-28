848e477491a0132b420e43e1eea6ef7c35ef0229
Phien <thaiphienn@gmail.com>
2023-10-28 21:59:47 +0700

    fix comment

--- src/components/Utils/AnswerFetch.js
+++ src/components/Utils/AnswerFetch.js
@@ -1,7 +1,7 @@
 import { shuffle } from "../Utils/Shuffle";
 
-//Fetch an answer from a QUIZ base on position
-//demo thisIsQuizArr = QuizFetch(data)   for(var i = 0; i < thisIsQuizArr.length; i++) thisIsAnAnswer = answerFetch(thisIsQuizArr, i)
+//Fetch 4 answer from a QUIZ base on position
+//demo thisIsQuizArr = QuizFetch(data)   for(var i = 0; i < thisIsQuizArr.length; i++) thisIsAnswers = answerFetch(thisIsQuizArr, i)
 //Version 1.0.0.0
 
 export function answerFetch(quiz, pos) {
