582972fc74b5144281fcce61e38f90a37c2ae56f
Phien <thaiphienn@gmail.com>
2023-10-28 21:00:22 +0700

    add comment

--- src/components/Utils/AnswerFetch.js
+++ src/components/Utils/AnswerFetch.js
@@ -1,7 +1,7 @@
 import { shuffle } from "../Utils/Shuffle";
 
-//Fetch answers from a quiz 
-//demo 
+//Fetch an answer from a QUIZ base on position
+//demo thisIsQuizArr = QuizFetch(data)   for(var i = 0; i < thisIsQuizArr.length; i++) thisIsAnAnswer = answerFetch(thisIsQuizArr, i)
 
 export function answerFetch(quiz, pos) {
   let res = shuffle(quiz[pos].answer);
