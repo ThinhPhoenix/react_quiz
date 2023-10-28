4c0668c5751fd786437c7393e27f3369e8916bbf
Phien <thaiphienn@gmail.com>
2023-10-28 21:16:33 +0700

    fix shuffle

--- src/components/Utils/Shuffle.js
+++ src/components/Utils/Shuffle.js
@@ -1,6 +1,6 @@
 // Shuffle Element in an array sourceArray
 // demo :  arr = [1,2,3,4,5];   shuffle(arr); console.log(arr) -> 2,5,1,3,2
-// Version 1.0.0.0
+// Version 1.0.0.1
 
 export function shuffle(sourceArray) {
   for (var i = 0; i < sourceArray.length - 1; i++) {
