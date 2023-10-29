96c29a09a898c7e02e4470b1333a9d464da95ae6
Phien <thaiphienn@gmail.com>
2023-10-29 23:22:28 +0700

    update QuizFetch

--- src/components/Utils/QuizFetch.js
+++ src/components/Utils/QuizFetch.js
@@ -2,7 +2,7 @@ import { shuffle } from "../Utils/Shuffle";
 
 //Fetch Quizzes in data
 //demo QuizFetch(data) -> [quiz1, quiz2, quiz3, ...]
-//Version 1.0.0.0
+//Version 1.1.0.0
 
 export function QuizFetch(data) {
   let res = shuffle(Object.keys(data.lsQuizz));
