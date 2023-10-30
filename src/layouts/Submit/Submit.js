3bfe06cdcdae8cf95deebc6c1cd322cf310a797f
ThinhPhoenix <laithinh1310@gmail.com>
2023-10-30 21:55:00 +0700

    sdasd

--- src/layouts/Submit/Submit.js
+++ src/layouts/Submit/Submit.js
@@ -3,74 +3,70 @@ import { useNavigate } from "react-router-dom";
 import { ctx } from "../../CtxData";
 import "./Submit.scss";
 
-function convertDataToNewFormat(savedData) {
-  const convertedData = [];
-
-  for (const quizId in savedData.lsAns) {
-    const choices = savedData.lsAns[quizId].choice;
-    choices.forEach((answerId) => {
-      convertedData.push({
-        quesId: quizId,
-        answerId,
-      });
-    }); // Add the missing closing parenthesis here
-  }
-
-  return convertedData;
-}
-
-
 export default function Submit() {
   let debug = true;
-  const [score,setScore] = useState(0)
+  const [score, setScore] = useState(0);
   const nav = useNavigate();
   const ctxDt = useContext(ctx);
   const userExamKey = `${ctxDt.user}_${ctxDt.examCode}`;
   const lsQuizId = ctxDt.Quiz.lsQuizz ? Object.keys(ctxDt.Quiz.lsQuizz) : [];
 
-  localStorage.setItem(`${ctxDt.user}_isSubmitted_${ctxDt.examCode}`, `true`);
+  localStorage.setItem(`${ctxDt.user}_isSubmitted_${ctxDt.examCode}`, "true");
 
   // Retrieve the time remaining from local storage
   const storedTimeRemaining = localStorage.getItem(
     `${ctxDt.user}_timeRemaining_${ctxDt.examCode}`
   );
 
-  // Create an object to store data for all quizzes
-  const allQuizData = {};
-
-  // Iterate through lsQuizId and fetch data for each quiz
-  lsQuizId.forEach((quizId) => {
-    const quizDataKey = `${userExamKey}_${quizId}`;
-    const quizDataJSON = localStorage.getItem(quizDataKey);
-
-    if (quizDataJSON) {
-      const quizData = JSON.parse(quizDataJSON);
-      allQuizData[quizId] = quizData;
-    } else {
-      allQuizData[quizId] = null;
-    }
-  });
-
   useEffect(() => {
-    const convertedData = convertDataToNewFormat(allQuizData);
-    
+    // Function to retrieve data for a quiz from local storage
+    const retrieveQuizData = (quizId) => {
+      const quizDataKey = `${userExamKey}_${quizId}`;
+      const quizDataJSON = localStorage.getItem(quizDataKey);
+
+      if (quizDataJSON) {
+        return JSON.parse(quizDataJSON);
+      } else {
+        return null;
+      }
+    };
+
+    // Create an array to store the data in the required format
+    const convertedData = [];
+
+    lsQuizId.forEach((quizId) => {
+      const quizData = retrieveQuizData(quizId);
+
+      if (quizData) {
+        const choices = quizData.lsAns[quizId].choice;
+        choices.forEach((answerId) => {
+          convertedData.push({
+            quesId: quizId,
+            answerId,
+          });
+        });
+      }
+    });
+
+    // Save the converted data in the required format to local storage (if needed)
+    localStorage.setItem("convertedData", JSON.stringify(convertedData));
+
     fetch("https://server.nglearns.com/answer/285498f5-3486-434d-a459-bedb6bcea7ce", {
       headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
+        "Accept": "application/json",
+        "Content-Type": "application/json",
       },
       method: "POST",
-      body: JSON.stringify(convertedData)
+      body: JSON.stringify(convertedData),
     })
-    .then((res) => res.json())
-    .then((dt) => {
-      console.log(dt);
-      setScore(dt)
-    });
+      .then((res) => res.json())
+      .then((dt) => {
+        console.log(dt);
+        setScore(dt);
+      });
   }, []);
-  
 
-  console.log("allQuizData:", allQuizData);
+  console.log("Score:", score);
 
   // Convert the stored time remaining (in seconds) to a number
   const storedTimeRemainingInSeconds = parseInt(storedTimeRemaining, 10);
@@ -93,24 +89,6 @@ export default function Submit() {
         <div>
           Debugging: <br />
           Exam ID: {ctxDt.examCode} <br />
-          {`${ctxDt.user}'s selected answers: `}
-          {Object.keys(allQuizData).map((quizId) => (
-            <div key={quizId}>
-              {allQuizData[quizId] ? (
-                <div>
-                  {Object.keys(allQuizData[quizId].lsAns).map((quizId) => (
-                    <div key={quizId}>
-                      Quiz ID: {quizId} <br />
-                      Choices:{" "}
-                      {allQuizData[quizId].lsAns[quizId].choice.join(", ")}
-                    </div>
-                  ))}
-                </div>
-              ) : (
-                <div>{/* /Blank */}</div>
-              )}
-            </div>
-          ))}
         </div>
       )}
     </div>
